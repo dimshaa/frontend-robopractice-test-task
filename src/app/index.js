b448c8a8cb13b5bcf3fc859c0ba7e4eaadb4af9c
Dmitry Shaposhnikov <ihatepost@gmail.com>
2022-09-18 20:06:51 +0300

    add MUI table

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,55 @@
+import api from "../utils/Api";
+
+import { useEffect, useState } from "react";
+import { Table, TableBody, TableCell, TableRow } from "@mui/material";
+
 export const App = () => {
-    return <h1>Hello</h1>
+  const [usersData, setUsersData] = useState([]);
+
+  useEffect(() => {
+    api.getData()
+      .then(users => {
+        console.log(users[0]);
+        setUsersData(users);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const strToMinutes = (str) => {
+    const hours = parseInt(str.split('-')[0]);
+    const minutes = parseInt(str.split('-')[1]);
+
+    return hours * 60 + minutes;
+  }
+
+  const spentTime = (start, end) => {
+    const time = strToMinutes(end) - strToMinutes(start);
+
+    return `${parseInt(time / 60)}:${time % 60}`
+  }
+
+  return (
+    <Table>
+      <TableBody>
+        {
+          usersData.map(user => {
+            return (
+              <TableRow key={user.id}>
+                <TableCell>
+                  {user.Fullname}
+                </TableCell>
+                {user.Days.map((day, index) => {
+                  return (
+                    <TableCell key={index}>
+                      {day ? spentTime(day.Start, day.End) : 0}
+                    </TableCell>
+                  )
+                })}
+              </TableRow>
+            )
+          })
+        }
+      </TableBody>
+    </Table>
+  );
 }
---
+++ src/utils/Api.js
@@ -0,0 +1,17 @@
+class Api {
+  getData() {
+    return fetch('http://localhost:8080/api/users', {
+      method: 'GET',
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Ошибка: ${res.status}`);
+      })
+  }
+};
+
+const api = new Api();
+
+export default api;
