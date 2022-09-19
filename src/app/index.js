2bfe3e882b1d482cb16df3c6b698b52a693acf26
Dmitry Shaposhnikov <ihatepost@gmail.com>
2022-09-19 10:20:14 +0300

    add table head

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,11 @@
 import api from "../utils/Api";
 
 import { useEffect, useState } from "react";
-import { Table, TableBody, TableCell, TableRow, TextField } from "@mui/material";
+import { Table, TableBody, TableCell, TableHead, TableRow, TextField } from "@mui/material";
 
 export const App = () => {
   const [usersData, setUsersData] = useState([]);
+  const [dataIsLoaded, setDataIsLoaded] = useState(false)
   const [query, setQuery] = useState('');
   const [usersDataToRender, setUsersDataToRender] = useState([])
 
@@ -12,6 +13,7 @@ export const App = () => {
     api.getData()
       .then(users => {
         setUsersData(users);
+        setDataIsLoaded(true);
       })
       .catch(err => console.log(err));
   }, []);
@@ -23,10 +25,7 @@ export const App = () => {
     return hours * 60 + minutes;
   }
 
-  const countSpentTime = (time) => {
-
-    return `${parseInt(time / 60)}:${time % 60}`;
-  }
+  const countSpentTime = (time) => `${parseInt(time / 60)}:${time % 60}`
 
   const mappedUserData = usersData.map(user => {
     let arr = new Array(31).fill(0);
@@ -51,16 +50,53 @@ export const App = () => {
   }
 
   useEffect(() => {
-    let result = mappedUserData.filter((user) => user.fullname.toLowerCase().includes(query));
+    let result = mappedUserData.filter(user => user.fullname.toLowerCase().includes(query));
     setUsersDataToRender(result);
-  }, [query, mappedUserData]);
+  }, [query, dataIsLoaded]);
 
   return (
     <>
     <TextField onChange={handleUserSearch} value={query} label="Search" variant="standard"></TextField>
     <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>User</TableCell>
+          <TableCell>1</TableCell>
+          <TableCell>2</TableCell>
+          <TableCell>3</TableCell>
+          <TableCell>4</TableCell>
+          <TableCell>5</TableCell>
+          <TableCell>6</TableCell>
+          <TableCell>7</TableCell>
+          <TableCell>8</TableCell>
+          <TableCell>9</TableCell>
+          <TableCell>10</TableCell>
+          <TableCell>11</TableCell>
+          <TableCell>12</TableCell>
+          <TableCell>13</TableCell>
+          <TableCell>14</TableCell>
+          <TableCell>15</TableCell>
+          <TableCell>16</TableCell>
+          <TableCell>17</TableCell>
+          <TableCell>18</TableCell>
+          <TableCell>19</TableCell>
+          <TableCell>20</TableCell>
+          <TableCell>21</TableCell>
+          <TableCell>22</TableCell>
+          <TableCell>23</TableCell>
+          <TableCell>24</TableCell>
+          <TableCell>25</TableCell>
+          <TableCell>26</TableCell>
+          <TableCell>27</TableCell>
+          <TableCell>28</TableCell>
+          <TableCell>29</TableCell>
+          <TableCell>30</TableCell>
+          <TableCell>31</TableCell>
+          <TableCell>Montly Total</TableCell>
+        </TableRow>
+      </TableHead>
       <TableBody>
-        {usersDataToRender &&
+        {dataIsLoaded &&
           usersDataToRender.map(user => {
             return (
               <TableRow key={user.id}>
